1a22190f96e5ce1536b3211670af3370dad9deab
vaggelisdel <delliosvaggelis@gmail.com>
2020-10-14 12:33:11 +0300

    Create an AWS EC2 Instance with Ngnix

---
+++ script.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get -y install nginx
+
+# make sure nginx is started
+service nginx start
---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-013da1cc4ae87618c"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
